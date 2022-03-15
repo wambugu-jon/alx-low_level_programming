94c0356e5dfd4caad8f3a13499e646447978a682
wambugu-jon <mbariwambugu@gmail.com>
2022-03-15 14:29:44 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * program that prints _putchar, followed by a new line.
+ * program should return 0.
+ */
+
+int main (void)
+{
+	_putchar('_putchar');
+	_putchar('\n');
+
+	return (0);
+}
