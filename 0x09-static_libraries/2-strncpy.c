09add50393340cf5da4b8d82dfa68c59f8dded15
wambugu-jon <mbariwambugu@gmail.com>
2022-04-06 01:53:38 -0700

    static library libmy.a containing all the functions.

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy two strings only taking n bytes from src
+ * @dest: first string to be added to
+ * @src: second string to be added
+ * Return: concatenated string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *temp = dest;
+
+	for (; n != 0 && *src != '\0'; n--, src++)
+	{
+		*temp = *src;
+		temp++;
+	}
+	for (; n != 0 && *temp != '\0'; n--, temp++)
+		*temp = '\0';
+	return (dest);
+}
