d718bd382c32b1e70b958f3584b15edd62e2d9db
wambugu-jon <mbariwambugu@gmail.com>
2022-04-11 13:55:29 -0700

    header file that defines a macro named PI as an abbreviation for the token 3.14159265359.

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI
+#define PI
+
+/**
+ *PI - abbreviation for the token
+ *
+ *Description: Header file that defines a macro named
+ *             PI as an abbreviation for the token 3.14159265359
+ */
+
+#define PI 3.14159265359
+
+#endif
