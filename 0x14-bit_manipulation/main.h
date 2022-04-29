39f01a064c714c3af5e2f471b5592446f276b20e
Wambugu Jon <mbariwambugu@gmail.com>
2022-04-29 08:25:58 +0300

    Correction to main header file.

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -15,8 +15,6 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 int _strlen(const char *s);
-_Bool isbin(char);
 unsigned int binary_to_uint(const char *b);
-
 
 #endif
